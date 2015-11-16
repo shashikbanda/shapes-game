111217df549b1bb7edf5a272a2f627485c7bae74
Shashik Banda <shashik@Shashiks-MacBook-Pro.local>
2015-11-16 10:18:48 -0800

    re-intial folder

---
+++ js/shapes.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+	createGrid();
+}
+
+function createGrid(){
+	console.log(GLOBALS['x-value']);
+}
+
+//create login html / css
+//create gameboard html / css
+
+//add clickable objects to gameboard / js
+//add dynamic scoreboard 
+
+
+
